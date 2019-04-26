6369c780c5fa1f3d80b7e9c3fad447d2f4cbec7c
mac <chunxue3@illinois.edu>
2019-04-25 22:55:04 -0500

    hjkj

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.illinois.cs.cs125.spring2019.lab12;
 
+/*
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 }
-/**
+*/
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -42,15 +44,19 @@ import org.json.JSONObject;
 /**
  * Main class for our UI design lab.
  */
-/*public final class MainActivity extends AppCompatActivity {
+public final class MainActivity extends AppCompatActivity {
     /**
      * Default logging tag for messages from the main activity.
      */
-    /*private static final String TAG = "Lab12:Main";
+    private static final String TAG = "Lab12:Main";
 
     private Button btn1;
-    private EditText editText;
+    private EditText city;
+    private EditText state;
     private TextView textView;
+    private double latitude;
+    private double longtitude;
+    private String key = "AIzaSyAwRH6Junl7BV235LakrC3jzV7fCQeEtDg";
 
     /** Request queue for our API requests. */
     /*private static RequestQueue requestQueue;
@@ -61,50 +67,56 @@ import org.json.JSONObject;
      * @param savedInstanceState unused
      */
     @Override
-    /*protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1 = (Button) findViewById(R.id.enter1);
-        editText = (EditText) findViewById(R.id.enterNumber);
+        city = (EditText) findViewById(R.id.city_name);
+        state = (EditText) findViewById(R.id.state_name);
         textView = (TextView) findViewById(R.id.outputDisplay);
 
         // Set up the queue for our API requests
+        RequestQueue requestQueue;
         requestQueue = Volley.newRequestQueue(this);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 try {
+                    final String findplace_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + city.getText().toString() + "%20" + state.getText().toString() + "&inputtype=textquery&fields=geometry&key=" + key;
+                    Log.d("here", findplace_url);
                     JsonObjectRequest request = new JsonObjectRequest(
                             Request.Method.GET,
-                            "http://numbersapi.com/" + editText.getText().toString() + "/trivia?notfound=floor&fragment",
+                            findplace_url,
                             null,
                             new Response.Listener<JSONObject>() {
                                 @Override
                                 public void onResponse(final JSONObject response) {
                                     try {
-                                        String c = b.getString("text");
-                                        textView.setText(c);
+                                        JSONObject abc = response.getJSONArray("candidates").getJSONObject(0);
+                                        JSONObject geometry = (JSONObject) abc.get("geometry");
+                                        JSONObject location = (JSONObject) geometry.get("location");
+                                        latitude = location.getDouble("lat");
+                                        longtitude = location.getDouble("lng");
+                                        textView.setText(Double.toString(longtitude) + "," + Double.toString(latitude));
                                     } catch (JSONException e) {
                                         e.printStackTrace();
-                                        textView.setText("Got error, ");
                                     }
                                 }
                             }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(final VolleyError error) {
-                            Log.e(TAG, "error.");
-                            textView.setText("Got error, ");
+                            Log.e(TAG, error.toString());
+
                         }
                     });
+                    requestQueue.add(request);
                 } catch (Exception e) {
                     e.printStackTrace();
-                    textView.setText("Got error, ");
                 }
             }
         });
     }
 }
-*/
 
 /**
  * private void sendRequestAndPrintReponse1(final TextView textView, final EditText editText) {
